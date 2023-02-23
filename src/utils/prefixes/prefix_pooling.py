7929efa92b493441d49bac6c71a79d8c90c9920c
Martina Rossini <martina.rossini704@gmail.com>
2023-02-23 19:13:40 +0100

    Fixed bug in prefix pooling code for encoder-decoder models

--- src/utils/prefixes/prefix_pooling.py
+++ src/utils/prefixes/prefix_pooling.py
@@ -125,6 +125,9 @@ class PrefixEncoderWithPromptPool(nn.Module):
         
         input_dep_prefix = self.prompt_pool(inputs_embeds)
         input_dep_prefix = self.pool_dropout(self.trans(input_dep_prefix))
+        if inputs_embeds.shape[0] < past_key_values.shape[0]:
+            input_dep_prefix = input_dep_prefix.repeat(past_key_values.shape[0]//inputs_embeds.shape[0], 1, 1)
+
         past_key_values = torch.cat([past_key_values, input_dep_prefix], dim=1)
         
         past_key_values = past_key_values.view(
